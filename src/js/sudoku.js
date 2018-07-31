9482671a5d25d567e21148fcd4661bbe0a6acc90
Annie Shin and Reese Glasscock <annie+reese@>
2018-07-31 16:47:58 -0700

    have method check to see if rows are unique with all digits

--- src/js/sudoku.js
+++ src/js/sudoku.js
@@ -10,3 +10,28 @@ Sudoku.prototype.checkIfAllNumbersEntered = function(){
     return false;
   }
 };
+
+Sudoku.prototype.checkRows = function(){
+  var row = []
+  var rows = []
+  for (var i = 0; i <= this.userAnswer.length; i+=9) {
+    for (var j = 0; j <= 8; j++) {
+      row.push(this.userAnswer[j])
+    }
+    rows.push(row);
+    console.log(row);
+    row = []
+  }
+
+  for (var k = 1; k <= 9; k++) {
+    for (var m = 0; m <= 9; m++) {
+
+      if (rows[m].includes(k)) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+  }
+}
